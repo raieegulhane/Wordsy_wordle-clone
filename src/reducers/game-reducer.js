9c90bd1e59426232da549052cb11e646dd808043
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2023-01-19 21:03:16 +0530

    add/remove letters + add guess

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -8,13 +8,15 @@ const initialGameState = {
         [{letter: "", color: ""}, {letter: "", color: ""}, {letter: "", color: ""}, {letter: "", color: ""}, {letter: "", color: ""}],
         [{letter: "", color: ""}, {letter: "", color: ""}, {letter: "", color: ""}, {letter: "", color: ""}, {letter: "", color: ""}]
     ],
+    currGuess: "",
     currGuessIndex: 0,
     currLetterIndex: 0,
-    inGameMessage: ""
+    inGameMessage: "",
+    gameOver: false
 }
 
 const gameReducerFunction = (state, { type, payload }) => {
-    const { guessesArray, currGuessIndex, currLetterIndex } = state;
+    const { currWord, guessesArray, currGuess, currGuessIndex, currLetterIndex } = state;
 
     switch (type) {
         case "INIT_GAME":
@@ -26,19 +28,66 @@ const gameReducerFunction = (state, { type, payload }) => {
         case "ADD_LETTER": 
             return({
                 ...state,
-                guessesArray: [ ...guessesArray ].map(
+                guessesArray: guessesArray.map(
                     (guess, i) => i === currGuessIndex ?
                     (guess.map((letterObj, j) => j === currLetterIndex ? { ...letterObj, letter: payload } : { ...letterObj })) : 
                     [ ...guess ]
                 ),
-                currLetterIndex: currLetterIndex < 5 ? currLetterIndex + 1 : 0
-            })
+                currGuess: currGuess + payload,
+                currLetterIndex: currLetterIndex + 1
+            });
+
+        case "REMOVE_LETTER":
+            return({
+                ...state,
+                guessesArray: guessesArray.map(
+                    (guess, i) => i === currGuessIndex ?
+                    (guess.map((letterObj, j) => j === currLetterIndex - 1 ? { letter: "", color: "" } : { ...letterObj })) :
+                    [ ...guess ]
+                ),
+                currGuess: currGuess.slice(0, currGuess.length - 1),
+                currLetterIndex: currLetterIndex - 1,
+            });
+
+        case "ADD_GUESS":
+            return({
+                ...state,
+                guessesArray: guessesArray.map(
+                    (guess, i) => i === currGuessIndex ?
+                    (guess.map((letterObj, j) => {
+                        if (currWord.includes(letterObj.letter)) {
+                            if (currWord[j] === currGuess[j]) {
+                                return { ...letterObj, color: "green" }
+                            }
+                            return { ...letterObj, color: "yellow" }
+                        }
+                        return { ...letterObj, color: "grey"}
+                    })) : 
+                    [ ...guess ]
+                ),
+                currGuess: "",
+                currGuessIndex: currGuessIndex + 1,
+                currLetterIndex: 0,
+            });
 
         case "GENERATE_MESSAGE":
             return({
                 ...state,
                 inGameMessage: payload
             });
+
+        case "RECORD_OUTCOME": 
+            return(
+                payload ? {
+                    ...state,
+                    inGameMessage: "YOU WON!!",
+                    gameOver: true
+                } : {
+                    ...state,
+                    inGameMessage: "Better luck next time",
+                    gameOver: true
+                }
+            );
 
         default:
             return initialGameState;
