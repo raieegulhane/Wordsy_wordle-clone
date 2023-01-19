b747700af34a1b7e2c9953c69f914ffa6dbeeded
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2023-01-19 21:04:03 +0530

    keyboard functions + color coding keys

--- src/components/keyboard/index.jsx
+++ src/components/keyboard/index.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { useGameContext } from "../../contexts";
+import { words } from "../../words";
 import "./keyboard.css";
 
 export const Keyboard = () => {
-    const { gameState: { currLetterIndex }, gameDispatch } = useGameContext();
-
-
+    const [greenKeysStr, setGreenKeysStr] = useState("");
+    const [usedKeysStr, setUsedKeysStr] = useState("");
+    const { 
+        gameState: { 
+            currWord, 
+            guessesArray, 
+            currGuess, 
+            currGuessIndex, 
+            currLetterIndex, 
+            gameOver 
+        }, 
+    gameDispatch } = useGameContext();
+    
     const qwertyLayout = [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L",],
@@ -13,23 +25,74 @@ export const Keyboard = () => {
 
     const addLetterHandler = (key) => {
         if (currLetterIndex === 5) {
-            return(gameDispatch({ type: "GENERATE_MESSAGE", payload: "The guess already has 5 letters"}));
+            return gameDispatch({ type: "GENERATE_MESSAGE", payload: "Guess can have 5 letters only"});
         }
+
         gameDispatch({ type: "ADD_LETTER", payload: key });
     }
 
-    const enterHandler = () => {
+    const removeLetterHandler = () => {
+        if (currLetterIndex === 0) {
+            return gameDispatch({ type: "GENERATE_MESSAGE", payload: "No letters"})
+        }
 
+        gameDispatch({ type: "REMOVE_LETTER" });
+    } 
+
+    const enterHandler = () => {
+        if (currLetterIndex < 5) {
+            return gameDispatch({ type: "GENERATE_MESSAGE", payload: "Not enough letters"});
+        }
+
+        if (!words.includes(currGuess.toLowerCase())) {
+            return gameDispatch({ type: "GENERATE_MESSAGE", payload: "Not in word list"});
+        }
+
+        if (currGuess === currWord) {
+            gameDispatch({ type: "RECORD_OUTCOME", payload: true })
+        }
+
+        if (currGuessIndex === 5) {
+            gameDispatch({ type: "RECORD_OUTCOME", payload: false });
+        }
+
+        gameDispatch({ type: "ADD_GUESS" });
     }
 
     const keyPressHandler = (key) => {
-        if (key === "ENTER") {
-            return enterHandler();
+        if (!gameOver) {
+            if (key === "ENTER") {
+                return enterHandler();
+            }
+
+            if (key === "BACKSPACE") {
+                return removeLetterHandler();
+            }
+    
+            return addLetterHandler(key);
+        }
+    }
+
+    useEffect(() => {
+        let greenKeys = "";
+        let usedKeys = ""
+
+        for (let i = 0; i < guessesArray.length; i++) {
+            if (i < currGuessIndex) {
+                for (let j = 0; j < guessesArray[i].length; j++) {
+                    const {letter, color } = guessesArray[i][j];
+                    if (color === "green") {
+                        greenKeys += letter;
+                    }
+                    usedKeys += letter
+                }
+            }
         }
 
-        return addLetterHandler(key);
-    }
-    
+        setGreenKeysStr(greenKeys);
+        setUsedKeysStr(usedKeys);
+    }, [currGuessIndex]);
+
     return(
         <div className="kb-wr u_fx-col u_fx-al-cn">
         {
@@ -41,7 +104,16 @@ export const Keyboard = () => {
                             return(
                                 <button 
                                     key={j} 
-                                    className="kb-key u_hov-scale"
+                                    className={`
+                                        kb-key u_hov-scale 
+                                        ${
+                                            usedKeysStr.includes(kbKey) ? (
+                                                currWord.includes(kbKey) ? (
+                                                    greenKeysStr.includes(kbKey) ? "kb-key-green" : "kb-key-yellow"
+                                                ) : "kb-key-used"
+                                            ) : "kb-key-unused"
+                                        }
+                                    `}
                                     onClick={() => keyPressHandler(kbKey)}
                                 >
                                 {
