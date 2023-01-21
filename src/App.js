86eda57879f4b394142244ad9de18f79a779601c
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2023-01-21 19:04:04 +0530

    light/dark theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import useLocalStorage from "use-local-storage";
 import "./styles/styles.css";
 import { WordsGrid, Keyboard, Navbar } from "./components";
 
 function App() {
+  const browserDefaultTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage("wordle-theme", browserDefaultTheme ? "dark" : "light");
+
   return (
-    <div className="App">
-      <Navbar />
+    <div className="App" data-theme={theme}>
+      <Navbar 
+        theme={theme}
+        setTheme={setTheme}
+      />
       <WordsGrid />
       <Keyboard />
     </div>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,9 @@
 import "./navbar.css";
 
-export const Navbar = () => {
+export const Navbar = ({ theme, setTheme }) => {
+    const toggleThemeHandler = () => {
+        setTheme(theme === "dark" ? "light" : "dark")
+    }
     return(
         <nav className="nav-wr u_fx-row u_fx-js-sb">
             <h1 className="nav-logo">Wordle</h1>
@@ -11,11 +14,15 @@ export const Navbar = () => {
                 <button className="nav-pill u_hov-scale">
                     <span className="material-icons">leaderboard</span>
                 </button>
-                <button className="nav-pill u_hov-scale">
-                    <span className="material-icons">dark_mode</span>
-                </button>
-                <button className="nav-pill u_hov-scale">
-                    <span className="material-icons">light_mode</span>
+                <button 
+                    className="nav-pill u_hov-scale"
+                    onClick={toggleThemeHandler}
+                >
+                    {
+                        theme === "dark" ? 
+                        <span className="material-icons">light_mode</span> :
+                        <span className="material-icons">dark_mode</span>
+                    }
                 </button>
             </section>
         </nav>
