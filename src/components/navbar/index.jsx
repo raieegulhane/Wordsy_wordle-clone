36e60db9b4985be0f6b5371ec9fe421249c80734
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2023-01-25 18:59:33 +0530

    css - navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -7,19 +7,19 @@ export const Navbar = ({ theme, setTheme, setShowModal }) => {
     
     return(
         <nav className="nav-wr u_fx-row u_fx-js-sb u_fx-al-cn">
-            <h1 className="nav-logo">Wordle</h1>
+            <h1 className="nav-logo">Wordsy</h1>
             <section className="nav-pills-wr u_fx-row u_fx-al-cn">
                 <button 
                     className="nav-pill u_hov-scale"
                     onClick={() => setShowModal({ show: true, modal: "INFO"})}
                 >
-                    <span className="material-icons nav-pill-icon">help</span>
+                    <span className="material-icons-round nav-pill-icon">help</span>
                 </button>
                 <button 
                     className="nav-pill u_hov-scale"
                     onClick={() => setShowModal({ show: true, modal: "STAT"})}
                 >
-                    <span className="material-icons nav-pill-icon">leaderboard</span>
+                    <span className="material-icons-round nav-pill-icon">leaderboard</span>
                 </button>
                 <button 
                     className="nav-pill"
@@ -27,8 +27,8 @@ export const Navbar = ({ theme, setTheme, setShowModal }) => {
                 >
                     {
                         theme === "dark" ? 
-                        <span className="material-icons nav-pill-icon btn-theme">light_mode</span> :
-                        <span className="material-icons nav-pill-icon btn-theme">dark_mode</span>
+                        <span className="material-icons-round nav-pill-icon btn-theme">light_mode</span> :
+                        <span className="material-icons-round nav-pill-icon btn-theme">dark_mode</span>
                     }
                 </button>             
             </section>
